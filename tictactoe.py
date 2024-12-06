Tashar02/tic-tac-toe

commit 1c53c6f3b752d43a36dfcd8a7142a699454c7784

tictactoe: Combine all winning line algorithms into one function
There's no need to make four different functions with tiny changes
to do the same thing, "drawing a straight line vertically, horizontally
or diagonally". Combine all of them into one function with an option
for an extra parameter to define which type of straight line should
be drawn. This saves us from some redundant code.

Also, make the "row" parameter defaulted to "None" since diagonals
don't need any calculation associated with the row number. We can
override the row value for vertical and horizontal line explicitly.

Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

tictactoe.py
@@ -57,31 +57,20 @@ def drawXO():
 		screen.blit(img, (col * CELL_SIZE + SPACE // 2, row * CELL_SIZE + SPACE // 2))
 	game_data['updated_cells'].clear()
 
-# draw_vertical_winning_line() - Draws a vertical line for the winner.
+# draw_winning_line() - Draws winning lines for the winner.
 # Returns: None
-def draw_vertical_winning_line(col, player):
-	posX = col * CELL_SIZE + CELL_SIZE // 2
+def draw_winning_line(player, line_type, row = None):
 	color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
-	pg.draw.line(screen, color, (posX, 15), (posX, HEIGHT - 15), 15)
-
-# draw_horizontal_winning_line() - Draws a horizontal line for the winner.
-# Returns: None
-def draw_horizontal_winning_line(row, player):
-	posY = row * CELL_SIZE + CELL_SIZE // 2
-	color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
-	pg.draw.line(screen, color, (15, posY), (WIDTH - 15, posY), 15)
-
-# draw_asc_diagonal() - Draws a diagonal line from bottom-left to top-right for the winner.
-# Returns: None
-def draw_asc_diagonal(player):
-	color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
-	pg.draw.line(screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), 15)
-
-# draw_desc_diagonal() - Draws a diagonal line from top-left to bottom-right for the winner.
-# Returns: None
-def draw_desc_diagonal(player):
-	color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
-	pg.draw.line(screen, color, (15, 15), (WIDTH - 15, HEIGHT - 15), 15)
+	if line_type == 'vertical':
+		posX = col * CELL_SIZE + CELL_SIZE // 2
+		pg.draw.line(screen, color, (posX, 15), (posX, HEIGHT - 15), 15)
+	elif line_type == 'horizontal':
+		posY = row * CELL_SIZE + CELL_SIZE // 2
+		pg.draw.line(screen, color, (15, posY), (WIDTH - 15, posY), 15)
+	elif line_type == 'asc_diagonal':
+		pg.draw.line(screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), LINE_WIDTH)
+	elif line_type == 'desc_diagonal':
+		pg.draw.line(screen, color, (15, 15), (WIDTH - 15, HEIGHT - 15), LINE_WIDTH)
 
 # check_winner() - Checks if there's a winner and draws the winning line.
 # Returns: 'X' or 'O' if there's a winner, None otherwise.
@@ -89,18 +78,18 @@ def check_winner():
 	# Check for a winner in rows and columns
 	for i in range(BOARD_ROWS):
 		if game_data['board'][i][0] == game_data['board'][i][1] == game_data['board'][i][2] and game_data['board'][i][0]:
-			draw_horizontal_winning_line(i, game_data['board'][i][0])
+			draw_winning_line(game_data['board'][i][0], 'horizontal', i)
 			return game_data['board'][i][0]
 		if game_data['board'][0][i] == game_data['board'][1][i] == game_data['board'][2][i] and game_data['board'][0][i]:
-			draw_vertical_winning_line(i, game_data['board'][0][i])
+			draw_winning_line(game_data['board'][0][i], 'vertical', i)
 			return game_data['board'][0][i]
 
 	# Check for a winner in diagonals
 	if game_data['board'][0][0] == game_data['board'][1][1] == game_data['board'][2][2] and game_data['board'][0][0]:
-		draw_desc_diagonal(game_data['board'][0][0])
+		draw_winning_line(game_data['board'][0][0], 'desc_diagonal')
 		return game_data['board'][0][0]
 	if game_data['board'][0][2] == game_data['board'][1][1] == game_data['board'][2][0] and game_data['board'][0][2]:
-		draw_asc_diagonal(game_data['board'][0][2])
+		draw_winning_line(game_data['board'][0][2], 'asc_diagonal')
 		return game_data['board'][0][2]
 
 	return None

commit 5da7612bf01b9dfb9bd41ae834f7b21b49cfa784

tictactoe: Remove undeclared "col" usage in draw_winning_line()
I forgot to remove col while implementing commit 1c53c6f3b75 ("tictactoe:
Combine all winning line algorithms into one function"). Since the position
for posX and posY would be the same if I were to replace "col" with "row",
just remove the variable usage all together and directly calculate inside
pg.draw.line().
While at it, edit "row" parameter of draw_winning_line() to "pos" to match
our intention.

This fixes the following error when we try to win vertically:

Traceback (most recent call last):
  File "/home/tashar/tic-tac-toe/tictactoe.py", line 180, in <module>
    event_handler()
  File "/home/tashar/tic-tac-toe/tictactoe.py", line 160, in event_handler
    user_click()
  File "/home/tashar/tic-tac-toe/tictactoe.py", line 131, in user_click
    if check_winner() or check_draw():
       ^^^^^^^^^^^^^^
  File "/home/tashar/tic-tac-toe/tictactoe.py", line 84, in check_winner
    draw_winning_line(game_data['board'][0][i], 'vertical', i)
  File "/home/tashar/tic-tac-toe/tictactoe.py", line 65, in draw_winning_line
    posX = col * CELL_SIZE + CELL_SIZE // 2
           ^^^
NameError: name 'col' is not defined

Fixes: 1c53c6f3b75 ("tictactoe: Combine all winning line algorithms into one function")
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

tictactoe.py
@@ -59,14 +59,12 @@ def drawXO():
 
 # draw_winning_line() - Draws winning lines for the winner.
 # Returns: None
-def draw_winning_line(player, line_type, row = None):
+def draw_winning_line(player, line_type, pos = None):
 	color = CIRCLE_COLOR if player == 'O' else CROSS_COLOR
 	if line_type == 'vertical':
-		posX = col * CELL_SIZE + CELL_SIZE // 2
-		pg.draw.line(screen, color, (posX, 15), (posX, HEIGHT - 15), 15)
+		pg.draw.line(screen, color, (pos * CELL_SIZE + CELL_SIZE // 2, 15), (pos * CELL_SIZE + CELL_SIZE // 2, HEIGHT - 15), LINE_WIDTH)
 	elif line_type == 'horizontal':
-		posY = row * CELL_SIZE + CELL_SIZE // 2
-		pg.draw.line(screen, color, (15, posY), (WIDTH - 15, posY), 15)
+		pg.draw.line(screen, color, (15, pos * CELL_SIZE + CELL_SIZE // 2), (WIDTH - 15, pos * CELL_SIZE + CELL_SIZE // 2), LINE_WIDTH)
 	elif line_type == 'asc_diagonal':
 		pg.draw.line(screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), LINE_WIDTH)
 	elif line_type == 'desc_diagonal':